f07bad18b1fc07ad7282542579fac0074ca7f19d
Nitin Kumar <nitinkumar6912@gmail.com>
2019-01-15 00:01:46 +0530

    jQuery, Bootstrap, Home
    Started working on the project.
    Add: bootstrap, jquery
    Add: HomeComponent
    
    Ready to work now.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoutesModule } from './routes/routes.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() {
+    console.log($(document).height());
+  }
+
+  ngOnInit() {
+  }
+
+}
